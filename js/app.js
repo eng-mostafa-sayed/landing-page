5f815554303af69921aa16a4bab8fc035785dd39
Mostafa sayed <ms519197@email.com>
2022-02-20 01:06:46 +0200

    finish highlighting

--- js/app.js
+++ js/app.js
@@ -59,7 +59,11 @@ var buildMyNavBar = function() {
         const listItem = document.createElement("li"); //creating the document fragment
         const SectionDataNav = sectionsNodeList[sec].getAttribute("data-nav");
         const sectionId = sectionsNodeList[sec].getAttribute("id");
-        listItem.innerHTML = `<a href="#${sectionId}" class="navLink">${SectionDataNav}</a>`; // adding <a> and customize the link
+
+        //we added the <a> with id represents the section, and two classes on refering to the section which will used dynamically highlighting and the text shown will be the section number
+        listItem.innerHTML = `<a href="#${sectionId}" class="navLink section${
+      sec + 1
+    }">${SectionDataNav}</a>`;
         documentFragment.appendChild(listItem);
     }
     myUlList.appendChild(documentFragment);
@@ -81,12 +85,21 @@ const highlighting = () => {
     for (var sec = 0; sec < sectionsNumber; sec++) {
         //detecting the section's top
         const sectionRectangle = sectionsNodeList[sec].getBoundingClientRect();
-        //if section on screen now change the style
-        if (sectionRectangle.top > 0 && sectionRectangle.top < 301) {
-            sectionsNodeList[sec].classList.add("active-now");
-            //if it gone away recover the style by removing the class "active-now"
-        } else {
-            sectionsNodeList[sec].classList.remove("active-now");
+        //the corresponding nav bar element for each section
+        const navElement = document.querySelector(`.section${sec + 1}`);
+        console.log(navElement);
+        //if section on screen now change the style of it and its corresponding nav element
+        if (sectionRectangle.top > -190 && sectionRectangle.top < 301) {
+            //now changing the section style
+            sectionsNodeList[sec].classList.add("active-now-section");
+            //change the corresponding navbar element style too
+            navElement.classList.add("active-now-nav");
+        }
+        //if it gone away recover the style by removing the class "active-now"
+        else {
+            sectionsNodeList[sec].classList.remove("active-now-section");
+            //change the corresponding navbar element style too
+            navElement.classList.remove("active-now-nav");
         }
     }
 };
